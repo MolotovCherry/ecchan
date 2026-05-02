b69137ff90f90b0f07568e2d7ed15c097935fcff
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2026-05-02 04:34:03 -0700

    Add workaround for bug not letting plugin import in qml https://github.com/KDAB/cxx-qt/issues/1433

--- ecchan-client-qml/build.rs
+++ ecchan-client-qml/build.rs
@@ -1,3 +1,5 @@
+use std::{env, path::PathBuf};
+
 use cxx_qt_build::{CxxQtBuilder, PluginType, QmlModule};
 
 fn main() {
@@ -6,4 +8,12 @@ fn main() {
     )
     .files(["src/ec_socket.rs"])
     .build();
+
+    // https://github.com/KDAB/cxx-qt/issues/1433
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let version_script = PathBuf::from(manifest_dir).join("qt-plugin.version");
+    println!(
+        "cargo::rustc-link-arg-cdylib=-Wl,--version-script={}",
+        version_script.display()
+    );
 }
