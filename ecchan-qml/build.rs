186e2160324249a36429ca80c05818795c267e3e
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2026-05-06 16:07:33 -0700

    Disable all cpp warnings

--- ecchan-qml/build.rs
+++ ecchan-qml/build.rs
@@ -5,11 +5,20 @@ use cxx_qt_build::{CxxQtBuilder, PluginType, QmlModule};
 fn main() {
     let manifest_dir: PathBuf = env::var("CARGO_MANIFEST_DIR").unwrap().into();
 
-    CxxQtBuilder::new_qml_module(QmlModule::new("ecchan_client").plugin_type(PluginType::Dynamic))
-        .files(["src/qml.rs", "src/qtlogging.rs", "src/qqml_property_map.rs"])
-        .cpp_file("src/cpp/qtlogging.cpp")
-        .include_dir(manifest_dir.join("includes/"))
-        .build();
+    let builder = CxxQtBuilder::new_qml_module(
+        QmlModule::new("ecchan_client").plugin_type(PluginType::Dynamic),
+    )
+    .files(["src/qml.rs", "src/qtlogging.rs", "src/qqml_property_map.rs"])
+    .cpp_file("src/cpp/qtlogging.cpp")
+    .include_dir(manifest_dir.join("includes/"));
+
+    let builder = unsafe {
+        builder.cc_builder(|cc| {
+            cc.warnings(false);
+        })
+    };
+
+    builder.build();
 
     // https://github.com/KDAB/cxx-qt/issues/1433
     let version_script = manifest_dir.join("qt-plugin.version");
