390e3f9f1366f270d9284ec6f9038ec9db9e85ae
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2026-05-09 10:27:39 -0700

    Remove unfulfilled lint expectation

--- ecchan-qml/src/cpp.rs
+++ ecchan-qml/src/cpp.rs
@@ -14,6 +14,5 @@ pub use jsserializer::JSEngineSerializer;
 pub use qjsengine::QJSEngine;
 pub use qjsvalue::QJSValue;
 pub use qjsvalueiterator::QJSValueIterator;
-#[expect(unused)]
 pub use qjsvaluelist::QJSValueList;
 pub use qqml_property_map::QQmlPropertyMap;
